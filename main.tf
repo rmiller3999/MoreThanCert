da757322f003cd6e087171f52ef4d8f77034b272
Rob M <rob@opsarray.com>
2023-11-16 10:35:45 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = "nodered/node-red:latest"
+
+  ports {
+    internal = 1880
+    external = 1880
+  }
+}
